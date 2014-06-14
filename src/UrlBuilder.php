d67effebaee5d02950f8dade163ca59837601734
Petr Grishin <petr.grishin@grishini.ru>
2014-06-14 22:25:18 +0400

    Реализация установки обязательных параметров

--- src/UrlBuilder.php
+++ src/UrlBuilder.php
@@ -7,6 +7,7 @@ namespace PetrGrishin\Url;
 
 
 use CUrlManager;
+use PetrGrishin\Url\Exception\UrlBuilderException;
 
 class UrlBuilder {
     const PARAMETER_NAME_HASH = '#';
@@ -17,11 +18,16 @@ class UrlBuilder {
     private $route;
     /** @var array */
     private $params = array();
+    /** @var array */
+    private $required = array();
 
     public static function className() {
         return get_called_class();
     }
 
+    /**
+     * @param CUrlManager $urlManager
+     */
     public function __construct(CUrlManager $urlManager) {
         $this->urlManager = $urlManager;
     }
@@ -65,6 +71,11 @@ class UrlBuilder {
         return $this;
     }
 
+    /**
+     * @param string $name
+     * @return mixed
+     * @throws Exception\UrlBuilderException
+     */
     public function getParam($name) {
         if (!array_key_exists($name, $this->params)) {
             throw new Exception\UrlBuilderException(sprintf('This param `%s` not exists'));
@@ -72,24 +83,57 @@ class UrlBuilder {
         return $this->params[$name];
     }
 
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return $this
+     */
     public function setParam($name, $value) {
         $this->params[$name] = $value;
         return $this;
     }
 
+    /**
+     * @return string
+     * @throws Exception\UrlBuilderException
+     */
     public function getHash() {
         return $this->getParam(self::PARAMETER_NAME_HASH);
     }
 
+    /**
+     * @param string $value
+     * @return $this
+     */
     public function setHash($value) {
         $this->setParam(self::PARAMETER_NAME_HASH, $value);
         return $this;
     }
 
     /**
+     * @return array
+     */
+    public function getRequired() {
+        return $this->required;
+    }
+
+    /**
+     * @param array $required
+     * @return $this
+     */
+    public function setRequired($required) {
+        $this->required = $required;
+        return $this;
+    }
+
+    /**
+     * @throws Exception\UrlBuilderException
      * @return string
      */
     public function getUrl() {
+        if ($this->hasRequiredParams()) {
+            throw new UrlBuilderException(sprintf('Required params `%s` not exists', implode(', ', $this->requiredParams())));
+        }
         return $this->getUrlManager()->createUrl($this->route, $this->params);
     }
 
@@ -100,7 +144,23 @@ class UrlBuilder {
         return array(
             'route' => $this->route,
             'params' => $this->params,
+            'required' => $this->required,
         );
+    }
+
+    /**
+     * @return UrlBuilder
+     */
+    public function copy() {
+        return clone $this;
+    }
+
+    protected function hasRequiredParams() {
+        return (boolean)$this->requiredParams();
+    }
+
+    protected function requiredParams() {
+        return array_diff($this->required, array_keys(array_filter($this->params)));
     }
 }
  
